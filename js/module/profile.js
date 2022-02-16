f2fba48e390aa443f84c334f43a45f8b26291ae4
Matheus Mendes <matheus_mendess@live.com>
2022-02-16 17:12:22 -0300

    editando meu perfil

--- js/module/profile.js
+++ js/module/profile.js
@@ -1,18 +1,27 @@
 export default function profile() {
-    fetch('https://api.opendota.com/api/players/159992731')
-        .then(res => res.json())
-        .then(profile => {
-            const myname = document.querySelector('.id');
-            const divProfile = createProfile(profile);
-            myname.appendChild(divProfile);
-            // myname.innerText = profile;
-            // console.log(profile);
-        })
-}
 
-function createProfile(profile) {
-    const div = document.createElement('div');
-    div.classList.add('.id');
-    div.innerHTML = `<h3>${profile}</h3>`;
-    return div;
 }
+fetch('https://api.opendota.com/api/players/159992731')
+.then(res => res.json())
+.then(json => {
+    const myavatar = document.querySelector('.avatar');
+    myavatar.innerHTML = 
+    `<img align="left" height="250" src="${json.profile.avatarfull}" />` + 
+    `<h3>
+        <span>NickName</span> <br>
+        ${json.profile.personaname}
+    </h3>` +
+    `<h4>
+        <span>Id Dota2</span> <br>
+        ${json.profile.account_id}
+    </h4>` +
+    `<h4>
+        <span>Localidade</span> <br>
+        ${json.profile.loccountrycode}
+    </h4>` +
+    `<h4>
+        <span>mmr estimado</span> <br>
+        ${json.mmr_estimate.estimate}
+    </h4>`
+    console.log(json.mmr_estimate.estimate); 
+})
