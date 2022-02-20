0975cd63a398d7168cfa408476c49005bf9745f3
matheusmendescosta <matheus_mendess@live.com>
2022-02-19 23:32:10 -0300

    trabalhando no modulo partidas recentes

--- js/module/recentMatches.js
+++ js/module/recentMatches.js
@@ -14,23 +14,37 @@ export default function recentMatches(){
 
     function createMatches(matches){
         const div = document.createElement('div');
-        div.classList.add('number-matches');
+        div.classList.add('table_recente_matches');
 
         div.innerHTML = 
         `
-        <tbody>
-            <tr>
-                <td>${matches.match_id}</td>
-                <td colspan="1">${matches.kills}</td>
-                <td colspan="1">${matches.deaths}</td>
-                <td colspan="1">${matches.assists}</td>
-                <td>${matches.xp_per_min}</td>
-                <td>${matches.gold_per_min}</td>
-                <td>${matches.hero_damage}</td>
-                <td>${matches.tower_damage}</td>
-            </tr>
-        </tbody>
-        </table>
+        <div>
+            <table>
+                <tbody>
+                   <tr>
+                        <th>Id partida</th>
+                        <th>K</th>
+                        <th>D</th>
+                        <th>A</th>
+                        <th>XP por Minuto</th>
+                        <th>Ouro por Minuto</th>
+                        <th>Dano em Heroi</th>
+                        <th>Dano em torre</th> 
+                    </tr>
+                    
+                    <tr>
+                        <td class="esquerda centro direita">${matches.match_id}</td>
+                        <td class="esquerda centro direita">${matches.kills}</td>
+                        <td class="esquerda centro direita">${matches.deaths}</td>
+                        <td class="esquerda centro direita">${matches.assists}</td>
+                        <td class="esquerda centro direita">${matches.xp_per_min}</td>
+                        <td class="esquerda centro direita">${matches.gold_per_min}</td>
+                        <td class="esquerda centro direita">${matches.hero_damage}</td>
+                        <td class="esquerda centro direita">${matches.tower_damage}</td>
+                    </tr>
+                </tbody>
+            </table>
+        <div>
         `;
         return div;
     }
