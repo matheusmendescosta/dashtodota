3e946ddd51ac8bf263d8bd7bfbdbd0a51ea36cd5
Matheus Mendes <matheus_mendess@live.com>
2022-03-01 14:18:03 -0300

    Montando todos os componentes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ContentInfo from "./components/ContentInfo/ContentInfo";
+import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
+import RecentMatches from "./components/RecentMatches/RecentMatches";
 
 const App = () => {
 
@@ -9,6 +11,8 @@ const App = () => {
         <Navbar />
         <Header />
         <ContentInfo />
+        <RecentMatches />
+        <Footer />
     </div>
 }
 
