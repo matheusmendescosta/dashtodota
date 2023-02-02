26c3217bd98bf19961c1a56a96f0f644be1f1ce2
Matheus Mendes <matheus_mendess@live.com>
2023-02-01 22:36:58 -0300

    FEAT finalizando

--- src/components/ContentInfo/ContentInfo.js
+++ src/components/ContentInfo/ContentInfo.js
@@ -3,7 +3,19 @@ import Api from "../../Api";
 
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-items: flex-start;
+`;
+
+export const BoxImage = styled.div`
+  margin: 2rem;
+`;
+
+export const BoxText = styled.div`
+  margin: 2rem;
+`;
 
 const ContentInfo = () => {
   const [heroes, setHeroes] = useState([]);
@@ -48,28 +60,55 @@ const ContentInfo = () => {
     setSugestion([]);
   };
 
+  console.log(suggestion);
+
   return (
-    <>
-      <div className="container">
-        <label for="exampleText0" class="form-label inline-block mb-2 text-gray-700">
-          Busque por um Herói
-        </label>
-        <input
-          class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-          id="exampleText0"
-          placeholder="Digite o nome do Herói"
-          onChange={(e) => onChangeHandler(e.target.value)}
-          value={text}
-        />
-      </div>
+    <div className="container">
+      <label for="exampleText0" className="form-label inline-block mb-2 text-gray-700">
+        Busque por um Herói
+      </label>
+      <input
+        class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+        id="exampleText0"
+        placeholder="Digite o nome do Herói"
+        onChange={(e) => onChangeHandler(e.target.value)}
+        value={text}
+      />
 
       {suggestion &&
         suggestion.map((sug, idx) => (
-          <div className="container" key={idx} onClick={() => suggestionHandler(sug.hero_id)}>
-            {sug.hero_id && sug.hero_id.length < 10 ? sug.hero_id : ""}
-          </div>
+          <>
+            <div className="container">
+              <div className="p-2 " key={idx} onClick={() => suggestionHandler(sug.hero_id)}>
+                {sug.hero_id && sug.hero_id.length < 20 ? sug.hero_id : ""}
+              </div>
+            </div>
+          </>
         ))}
-    </>
+      <p>
+        Busque informações específicas de cada herói, número de partidas, vitórias, derrotas entre outras informações
+      </p>
+
+      <Container>
+        <BoxImage>
+          {suggestion.map((hero) => (
+            <div>
+              <img src={"https://api.opendota.com" + hero.img} width={300} />
+            </div>
+          ))}
+        </BoxImage>
+        <BoxText>
+          {suggestion.map((hero) => (
+            <div>
+              <p>Total de jogos: {hero.games}</p>
+              <p>vitorias: {hero.against_games}</p>
+              <p>Derrotas: {hero.against_win}</p>
+              <p>Total de jogos: {hero.games}</p>
+            </div>
+          ))}{" "}
+        </BoxText>
+      </Container>
+    </div>
   );
 };
 
